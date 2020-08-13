c33851f64808c44e6eb8a9217eac20b23a313bdd
ciro.azevedo <ciro.azevedo@4all.com>
2020-08-13 14:36:16 -0300

    remove get

--- api.tf
+++ api.tf
@@ -25,22 +25,6 @@ resource "aws_api_gateway_integration" "this" {
   uri                     = "https://www.google.com"
 }
 
-resource "aws_api_gateway_method" "get" {
-  rest_api_id   = "${aws_api_gateway_rest_api.this.id}"
-  resource_id   = "${aws_api_gateway_resource.this.id}"
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "this" {
-  rest_api_id             = "${aws_api_gateway_rest_api.this.id}"
-  resource_id             = "${aws_api_gateway_resource.this.id}"
-  http_method             = "${aws_api_gateway_method.get.http_method}"
-  type                    = "HTTP"
-  integration_http_method = "GET"
-  uri                     = "https://jsonplaceholder.typicode.com/todos/1"
-}
-
 resource "aws_api_gateway_deployment" "this" {
   depends_on = ["aws_api_gateway_integration.this"]
 
