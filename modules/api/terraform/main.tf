ba3e4b7e92efd85e3be7eaf4af0bf876a5fb8fec
ciro.azevedo <ciro.azevedo@4all.com>
2020-08-17 10:47:53 -0300

    add new route json

--- modules/api/terraform/main.tf
+++ modules/api/terraform/main.tf
@@ -15,3 +15,15 @@ module "procura" {
   aws_api_terraform_id               = "${var.aws_api_terraform_id}"
   aws_api_terraform_root_resource_id = "${var.aws_api_terraform_root_resource_id}"
 }
+
+module "add" {
+  source                             = "./add"
+  aws_api_terraform_id               = "${var.aws_api_terraform_id}"
+  aws_api_terraform_root_resource_id = "${var.aws_api_terraform_root_resource_id}"
+}
+
+module "json" {
+  source                             = "./json"
+  aws_api_terraform_id               = "${var.aws_api_terraform_id}"
+  aws_api_terraform_root_resource_id = "${var.aws_api_terraform_root_resource_id}"
+}
